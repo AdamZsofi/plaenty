ac38f1e528c7714de5b3faf79f24e9953925d8f6
mondokm <milanmondok1998@gmail.com>
2021-11-28 15:30:26 +0100

    Improved config edit

--- client/app/src/main/java/hu/bme/aut/plaenty/ui/main/ConfigEditorActivity.java
+++ client/app/src/main/java/hu/bme/aut/plaenty/ui/main/ConfigEditorActivity.java
@@ -14,6 +14,7 @@ import hu.bme.aut.plaenty.R;
 import hu.bme.aut.plaenty.databinding.ActivityConfigEditorBinding;
 import hu.bme.aut.plaenty.databinding.ActivityMainBinding;
 import hu.bme.aut.plaenty.model.Configuration;
+import hu.bme.aut.plaenty.model.SensorData;
 import hu.bme.aut.plaenty.network.ConfigManager;
 import hu.bme.aut.plaenty.network.NetworkManager;
 
@@ -33,6 +34,16 @@ public class ConfigEditorActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         binding.spinner.setAdapter(adapter);
 
+        binding.ecSlider.addOnChangeListener(
+                (slider, value, fromUser) -> {
+                    binding.ecRangeText.setText(formatSensorData(binding.ecSlider.getValues().get(0)) + " - " + formatSensorData(binding.ecSlider.getValues().get(1)));
+                });
+
+        binding.phSlider.addOnChangeListener(
+                (slider, value, fromUser) -> {
+                    binding.phRangeText.setText(formatSensorData(binding.phSlider.getValues().get(0)) + " - " + formatSensorData(binding.phSlider.getValues().get(1)));
+                });
+
         Bundle bundle = getIntent().getExtras();
         Long configId = bundle.getLong("id");
 
@@ -47,15 +58,21 @@ public class ConfigEditorActivity extends AppCompatActivity {
 
     }
 
+    private String formatSensorData(double value){
+        return String.format("%.2f",value);
+    }
+
     private void refreshDisplay() {
         binding.configEditorName.setText(item.getName());
         binding.spinner.setSelection(item.getLightRequired().index);
         binding.phSlider.setValues((float) item.getPhmin(), (float) item.getPhmax());
         binding.ecSlider.setValues((float) item.getEcmin(), (float) item.getEcmax());
+        binding.onMinutes.setText(item.getPumpon()+"");
+        binding.offMinutes.setText(item.getPumpoff()+"");
     }
 
     public void setAsActive(View view) {
-        if(item == null) return;
+        if (item == null) return;
         ConfigManager.setActiveConfiguration(item,
                 () -> Snackbar.make(binding.getRoot(), R.string.set_active_error, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show());
