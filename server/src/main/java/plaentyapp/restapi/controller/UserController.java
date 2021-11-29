2041687278505852e6e53bde30f02b82b16cf23d
mondokm <milanmondok1998@gmail.com>
2021-11-29 11:00:41 +0100

    Removed unnecessary endpoints

--- server/src/main/java/plaentyapp/restapi/controller/UserController.java
+++ server/src/main/java/plaentyapp/restapi/controller/UserController.java
@@ -23,38 +23,11 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @GetMapping
-    public List<User> getAll() {
-        return userRepository.findAll();
-    }
-
-    @GetMapping("{id}")
-    public ResponseEntity<User> getById(@PathVariable long id) {
-        Optional<User> user = userRepository.findById(id);
-        if (!user.isPresent()){
-            return ResponseEntity.notFound().build();
-        } else {
-            return ResponseEntity.ok(user.get());
-        }
-    }
-
     @PostMapping("/sign-up")
     public ResponseEntity<?> create(@Valid @RequestBody User user) {
         if(userRepository.existsByName(user.getName())) return ResponseEntity.badRequest().build();
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return ResponseEntity.ok().build();
-    }
-
-    @DeleteMapping("{id}")
-    public ResponseEntity<?> delete(@PathVariable long id) {
-        Optional<User> user = userRepository.findById(id);
-        if (!user.isPresent()) {
-            return ResponseEntity.notFound().build();
-        }
-        else {
-            userRepository.deleteById(id);
-            return ResponseEntity.ok().build();
-        }
     }
 }
